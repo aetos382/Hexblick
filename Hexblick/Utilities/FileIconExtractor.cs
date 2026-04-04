8017acf1d21a37c95522899fb5498be7d0205db0
aetos <aetos382@users.noreply.github.com>
2026-04-04 15:11:01 +0100

    icon

--- Hexblick/Utilities/FileIconExtractor.cs
+++ Hexblick/Utilities/FileIconExtractor.cs
@@ -6,64 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 
-using Windows.Storage;
-using Windows.Storage.FileProperties;
-using Windows.Win32;
-using Windows.Win32.UI.Shell;
-
 namespace Hexblick.Utilities;
 
 internal static class FileIconExtractor
 {
-    public static async Task<ImageSource> GetFileIconAsync(string path)
+    public static async ValueTask<ImageSource?> GetFileIconAsync(string path)
     {
-        var storageFile = await StorageFile.GetFileFromPathAsync(path);
+        using var icon = Icon.ExtractAssociatedIcon(path);
+        if (icon is null)
+        {
+            // TODO: standard icon
+            return null;
+        }
 
-        using var thumbnail = await storageFile.GetScaledImageAsThumbnailAsync(
-            ThumbnailMode.SingleItem,
-            16,
-            ThumbnailOptions.None);
+        using var ms = new MemoryStream();
+        icon.Save(ms);
+        ms.Position = 0;
 
         var bitmap = new BitmapImage();
-
-        if (thumbnail.Type == ThumbnailType.Icon)
-        {
-            await bitmap.SetSourceAsync(thumbnail);
-        }
-        else
-        {
-            var imageBytes = await Task.Run(() =>
-            {
-                SHFILEINFOW sfi = default;
-
-                var result = PInvoke.SHGetFileInfo(
-                    path,
-                    0,
-                    ref sfi,
-                    SHGFI_FLAGS.SHGFI_ICON |
-                    SHGFI_FLAGS.SHGFI_SMALLICON |
-                    SHGFI_FLAGS.SHGFI_ICONLOCATION |
-                    SHGFI_FLAGS.SHGFI_SHELLICONSIZE |
-                    SHGFI_FLAGS.SHGFI_SYSICONINDEX);
-
-                try
-                {
-                    using var icon = Icon.FromHandle(sfi.hIcon);
-                    using var ms = new MemoryStream();
-
-                    icon.Save(ms);
-                    ms.Position = 0;
-                    return ms.ToArray();
-                }
-                finally
-                {
-                    PInvoke.DestroyIcon(sfi.hIcon);
-                }
-            });
-
-            using var ms = new MemoryStream(imageBytes);
-            await bitmap.SetSourceAsync(ms.AsRandomAccessStream());
-        }
+        await bitmap.SetSourceAsync(ms.AsRandomAccessStream());
 
         return bitmap;
     }
