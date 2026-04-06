04218b889d7d8706f23a427ebd045f7157ea2b3e
aetos <aetos382@users.noreply.github.com>
2026-04-06 19:16:57 +0900

    remove generator

--- ResourceGenerator/ReswResourceGenerator.cs
+++ ResourceGenerator/ReswResourceGenerator.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using System.IO;
+
+using Microsoft.CodeAnalysis;
 
 namespace ResourceGenerator;
 
@@ -10,5 +13,29 @@ public sealed class ReswResourceGenerator :
     public void Initialize(
         IncrementalGeneratorInitializationContext context)
     {
+        var priFilesProvider = context.AdditionalTextsProvider
+            .Where(static x => IsReswFile(x.Path))
+            .Select(static (file, cancellationToken) =>
+            {
+                var text = file.GetText(cancellationToken);
+                return (Path: file.Path, Text: text?.ToString());
+            })
+            .Collect()
+            .Select(static (input, cancellationToken) =>
+            {
+
+            });
+
+        context.RegisterSourceOutput(
+            priFilesProvider,
+            static (context, input) =>
+            {
+                Console.WriteLine(input.Path);
+            });
+    }
+
+    private static bool IsReswFile(string path)
+    {
+        return string.Equals(Path.GetExtension(path), ".resw", StringComparison.OrdinalIgnoreCase);
     }
 }
