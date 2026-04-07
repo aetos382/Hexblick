340f857532cff5c00eea3cedeecec770f6ee0424
aetos <aetos382@users.noreply.github.com>
2026-04-07 21:06:27 +0900

    wip

--- Hexblick/Program.cs
+++ Hexblick/Program.cs
@@ -34,8 +34,6 @@ services.AddScoped<IDocumentManager, DocumentManager>();
 services.AddScoped<IMultipleFileOpenPickerRequestHandler, MultipleFileOpenPickerRequestHandler>();
 services.AddScoped<IConfirmSaveRequesetHandler, ConfirmSaveRequestHandler>();
 
-services.AddScoped<EditorControl>();
-
 using var host = appBuilder.Build();
 
 await host.RunAsync().ConfigureAwait(false);
